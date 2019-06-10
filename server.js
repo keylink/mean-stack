591757a66900589e450b0fb05471524bf8b1b656
kanat <kana_t@list.ru>
2019-06-10 08:23:26 -0400

    server container start up

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/about', function (req, res) {
+  res.send('about');
+});
+
+app.get('/tes', function (req, res) {
+  res.send('tes');
+});
+
+app.listen(9090, function () {
+  console.log('Example app listening on port 9090!');
+});
